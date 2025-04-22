b499ad7af14d3b2bb443e23ec254f6ec99974e46
Healeraaa <chj20020305@126.com>
2025-04-22 12:40:38 +0800

    可与SquareLine联动，完成ui的设计与仿真

--- src/main.c
+++ src/main.c
@@ -1,14 +1,12 @@
 #include "porting/lv_porting.h"
+#include "GUI_APP/ui.h"
 #include "lvgl.h"
 
 int main(int argc, char const *argv[])
 {
     lv_porting_init();
 
-    lv_obj_t *label = lv_label_create(lv_scr_act());
-    lv_label_set_text(label, "Hello world!!!!!!");
-    lv_obj_center(label);
-
+    ui_init();
     while (1) {
         lv_timer_handler();
         lv_porting_delay();
